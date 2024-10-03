dca5ac6922336a61acc0473fe1f22c01ee902562
JeswinJJ2291 <jeswin.jose@jobinandjismi.com>
2024-10-03 16:07:32 +0530

    Update jj_cs_otp_7933.js

--- OTP-7536-InductionTraining/src/FileCabinet/SuiteScripts/JobinAndJismi/OTP-7536-InductionTraining/Task OTP-7933/jj_cs_otp_7933.js
+++ OTP-7536-InductionTraining/src/FileCabinet/SuiteScripts/JobinAndJismi/OTP-7536-InductionTraining/Task OTP-7933/jj_cs_otp_7933.js
@@ -1,4 +1,22 @@
 /**
+ *  client : Nil
+ *
+ *
+ *  OTP 7933 - Restrict IF Save
+ *
+ *
+ * ------------------------------------------------------------------------
+ *
+ * Author : Jobin And Jismi IT Services
+ *
+ * Date Created : 25 - September - 2024
+ *
+ * Description : This script is for displaying error message for item fulfillment if the customer deposit is below the total sales order amount. 
+ *
+ * REVISION HISTORY : 1.0
+ *
+ *
+ *
  * @NApiVersion 2.x
  * @NScriptType ClientScript
  * @NModuleScope SameAccount
@@ -8,9 +26,19 @@ define(['N/record', 'N/search', 'N/ui/message'],
      * @param{record} record
      * @param{search} search
      * @param{message} message
-     */
+     *///
     function (record, search, message) {
 
+        /**
+         * Calculates the total amount of customer deposits associated with a given sales order.
+        *
+         * This function searches for customer deposits linked to the specified sales order ID,
+        * summing the total amounts of those deposits that are not canceled. 
+         *
+        * @param {number} salesOrderId - The internal ID of the sales order for which to calculate the deposit total.
+        * @returns {number} The total amount of customer deposits related to the specified sales order.
+        * @throws {Error} If an error occurs while retrieving the deposit total.
+        */
         function depTotal(salesOrderId) {
             try {
                 var depositSearch = search.create({
@@ -35,8 +63,16 @@ define(['N/record', 'N/search', 'N/ui/message'],
                 console.log("Error in deposit" + e.message);
             }
         }
-
-
+        /**
+        * Retrieves the total amount of a specified sales order.
+        *
+        * This function loads the sales order record using its internal ID and retrieves
+        * the total amount from the sales order record.
+        *
+        * @param {number} salesOrderId - The internal ID of the sales order for which to retrieve the total.
+        * @returns {number} The total amount of the specified sales order.
+        * @throws {Error} If an error occurs while loading the sales order or retrieving the total.
+        */
         function salesOrderTotal(salesOrderId) {
             try {
                 // Load the Sales Order record
@@ -58,137 +94,6 @@ define(['N/record', 'N/search', 'N/ui/message'],
 
         }
 
-        /**
-         * Function to be executed after page is initialized.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
-         *
-         * @since 2015.2
-         */
-        function pageInit(scriptContext) {
-
-        }
-
-        /**
-         * Function to be executed when field is changed.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         * @param {string} scriptContext.fieldId - Field name
-         * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
-         * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
-         *
-         * @since 2015.2
-         */
-        function fieldChanged(scriptContext) {
-
-        }
-
-        /**
-         * Function to be executed when field is slaved.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         * @param {string} scriptContext.fieldId - Field name
-         *
-         * @since 2015.2
-         */
-        function postSourcing(scriptContext) {
-
-        }
-
-        /**
-         * Function to be executed after sublist is inserted, removed, or edited.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         *
-         * @since 2015.2
-         */
-        function sublistChanged(scriptContext) {
-
-        }
-
-        /**
-         * Function to be executed after line is selected.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         *
-         * @since 2015.2
-         */
-        function lineInit(scriptContext) {
-
-        }
-
-        /**
-         * Validation function to be executed when field is changed.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         * @param {string} scriptContext.fieldId - Field name
-         * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
-         * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
-         *
-         * @returns {boolean} Return true if field is valid
-         *
-         * @since 2015.2
-         */
-        function validateField(scriptContext) {
-
-        }
-
-        /**
-         * Validation function to be executed when sublist line is committed.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         *
-         * @returns {boolean} Return true if sublist line is valid
-         *
-         * @since 2015.2
-         */
-        function validateLine(scriptContext) {
-
-        }
-
-        /**
-         * Validation function to be executed when sublist line is inserted.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         *
-         * @returns {boolean} Return true if sublist line is valid
-         *
-         * @since 2015.2
-         */
-        function validateInsert(scriptContext) {
-
-        }
-
-        /**
-         * Validation function to be executed when record is deleted.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         *
-         * @returns {boolean} Return true if sublist line is valid
-         *
-         * @since 2015.2
-         */
-        function validateDelete(scriptContext) {
-
-        }
 
         /**
          * Validation function to be executed when record is saved.
@@ -228,15 +133,7 @@ define(['N/record', 'N/search', 'N/ui/message'],
         }
 
         return {
-            // pageInit: pageInit,
-            // fieldChanged: fieldChanged,
-            // postSourcing: postSourcing,
-            // sublistChanged: sublistChanged,
-            // lineInit: lineInit,
-            // validateField: validateField,
-            // validateLine: validateLine,
-            // validateInsert: validateInsert,
-            // validateDelete: validateDelete,
+
             saveRecord: saveRecord
         };
 
